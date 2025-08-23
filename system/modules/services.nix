afc57d621af82b74d19423b51b892cd5e39707e6
mato <mato.kollar2004@gmail.com>
2025-08-23 10:59:32 +0200

    Added sk keyboard variant

--- system/modules/services.nix
+++ system/modules/services.nix
@@ -2,24 +2,24 @@
 
 {
   services = {
-		dbus.enable = true;
-		openssh.enable = true;
-
-		displayManager = {
-			sddm = {
-				enable = true;
-				theme = "catppuccin-mocha";
-  				package = pkgs.kdePackages.sddm;
-			};
-			defaultSession = "none+i3";
-		};
-		xserver = {
-			enable = true;
-			windowManager.i3.enable = true;
-			xkb = {
-				layout = "us";
-			};
-			# videoDrivers = [ "amdgpu" "virtualbox" ];
-		};
+    dbus.enable = true;
+    openssh.enable = true;
+    displayManager = {
+      sddm = {
+        enable = true;
+        theme = "catppuccin-mocha";
+        package = pkgs.kdePackages.sddm;
+      };
+      defaultSession = "none+i3";
+    };
+    xserver = {
+      enable = true;
+      windowManager.i3.enable = true;
+      xkb = {
+        layout = "us,sk";
+        options = "grp:alt_shift_toggle,caps:swapescape";
+      };
+      # videoDrivers = [ "amdgpu" "virtualbox" ];
+    };
   };
 }
