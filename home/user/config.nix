8621685a84c9a13835aef3e2de6482306b7a5725
mato <mato.kollar2004@gmail.com>
2025-09-29 14:42:16 +0200

    Stylix color theme experimenting

--- home/user/config.nix
+++ home/user/config.nix
@@ -7,13 +7,14 @@ in
   home.file = {
     ".config/i3".source = "${configDir}/i3";
     #".config/kitty".source = "${configDir}/kitty";
-    ".config/polybar".source = "${configDir}/polybar";
+    # ".config/polybar".source = "${configDir}/polybar";
     ".config/swayidle".source = "${configDir}/swayidle";
     ".config/swaylock".source = "${configDir}/swaylock";
     ".config/wlogout".source = "${configDir}/wlogout";
     ".config/btop".source = "${configDir}/btop";
     ".config/rofi".source = "${configDir}/rofi";
     ".config/mako".source = "${configDir}/mako";
+    ".config/themes".source = "${configDir}/themes";
   };
 }
 
--- home/user/stylix.nix
+++ home/user/stylix.nix
@@ -3,9 +3,27 @@
 {
   stylix = {
     enable = true;
-    # image = ./home/themes/space_panels.png;
+    image = ../config/themes/space_panels.png;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+    base16Scheme = {
+      base00 = "#0c0c1c"; #06060e
+      base01 = "#121229"; #0d0c1b
+      base02 = "#131229";
+      base03 = "#9796a4";
+      base04 = "#a1a1ad";
+      base05 = "#8380c6"; #b68d5d
+      base06 = "#d7d7ed";
+      base07 = "#f2f2f9";
+      base08 = "#605db6";
+      base09 = "#b3b65d";
+      base0A = "#5db3b6";
+      base0B = "#b6605d";
+      base0C = "#87b65d";
+      base0D = "#8380c6";
+      base0E = "#b68d5d"; #b0aedb
+      base0F = "#5d87b6";
+    };
 
     cursor = {
       package = pkgs.bibata-cursors;
